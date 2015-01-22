8e328cf53902494128030bb2deaca2375eff1423
Jeff Jacob <jeff.jacob@nanaimo.ca>
2015-01-22 14:57:59 -0800

    Day 1 Concept of a Cube with Sides

---
+++ Rubiks/Models/Cube.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace Rubiks.Models
+{
+    public class Cube
+    {
+        private DataTable _sides;
+
+        public DataTable sides
+        {
+            get { return _sides; }
+            set { _sides = value; }
+        }
+
+        public void RotateFlipType(Rotations direction)
+        {
+            
+        }
+
+        public void Solve()
+        {
+            RotateFlipType(Rotations.Middle);
+        }
+    }
+
+    //Rotations are to the right or down
+    public enum Rotations {Top, Middle, Bottom, Left, Center, Right, Front, Inner, Back }
+
+}
